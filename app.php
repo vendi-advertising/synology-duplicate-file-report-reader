278a9f8ecf4451f87bc3455522e5c1d09dc0b86a
cjhaas <chris@vendiadvertising.com>
2017-08-03 14:49:41 -0500

    Register a default and single command

--- app.php
+++ app.php
@@ -2,10 +2,13 @@
 
 define( 'VENDI_ADMIN_SYNOLOGY_DUPE_FILE', __FILE__ );
 define( 'VENDI_ADMIN_SYNOLOGY_DUPE_PATH', dirname( __FILE__ ) );
-define( 'VENDI_ADMIN_SYNOLOGY_DUPE_APP_VERSION', '0.0.1' );
+define( 'VENDI_ADMIN_SYNOLOGY_DUPE_APP_VERSION', '0.0.2' );
 
 require_once VENDI_ADMIN_SYNOLOGY_DUPE_PATH . '/vendor/autoload.php';
 
+$cmd = new Vendi\Admin\Synology\DuplicateFiles\DupeLogReader();
+
 $application = new Symfony\Component\Console\Application( 'Vendi Admin - Synology Duplicate File Report Reader', VENDI_ADMIN_SYNOLOGY_DUPE_APP_VERSION );
-$application->add( new Vendi\Admin\Synology\DuplicateFiles\DupeLogReader() );
+$application->add( $cmd );
+$application->setDefaultCommand( $cmd->getName(), true );
 $application->run();
